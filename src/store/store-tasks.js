635fcbd24ffff5e11c59cf766ec4f7bf2c007d75
lucas7796 <lucas.pineda@hotmail.com>
2020-04-22 23:39:29 -0300

    use actions and mutations methods to do and undo tasks

--- src/store/store-tasks.js
+++ src/store/store-tasks.js
@@ -1,3 +1,5 @@
+import Vue from 'vue'
+
 const state = {
 	tasks: {
 		'ID1': {
@@ -22,11 +24,21 @@ const state = {
 }
 
 const mutations = {
-
+	updateTask(state, payload) {
+		Object.assign(state.tasks[payload.id], payload.updates)
+	},
+	deleteTask(state, id) {
+		Vue.delete(state.tasks, id)
+	}
 }
 
 const actions = {
-
+	updateTask({ commit }, payload) {
+		commit('updateTask', payload)
+	},
+	deleteTask({ commit }, id) {
+		commit('deleteTask', id)
+	}
 }
 
 const getters = {
