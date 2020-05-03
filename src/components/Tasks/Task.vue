10aaf61836d79eb0c424dd25c19adb18f983031b
lucas7796 <lucas.pineda@hotmail.com>
2020-05-02 21:29:49 -0300

    implement edittask

--- src/components/Tasks/Task.vue
+++ src/components/Tasks/Task.vue
@@ -1,9 +1,9 @@
 <template>
   <q-item 
-    @click="updateTask({ id: id, updates: { completed: !task.completed } })"
-    :class="!task.completed ? 'bg-orange-1' : 'bg-green-1'"
-    clickable
-    v-ripple>
+  	@click="updateTask({ id: id, updates: { completed: !task.completed } })"
+  	:class="!task.completed ? 'bg-orange-1' : 'bg-green-1'"
+  	clickable
+  	v-ripple>
     <q-item-section side top>
       <q-checkbox 
         v-model="task.completed"
@@ -12,43 +12,61 @@
 
     <q-item-section>
       <q-item-label
-        :class="{ 'text-strikethrough' : task.completed }">
-        {{ task.name }}
+      	:class="{ 'text-strikethrough' : task.completed }">
+      	{{ task.name }}
       </q-item-label>
+    </q-item-section>
+
+    <q-item-section 
+      v-if="task.dueDate"
+      side>
+    	<div class="row">
+    		<div class="column justify-center">
+		    	<q-icon 
+		    		name="event"
+		    		size="18px"
+		    		class="q-mr-xs" />
+    		</div>
+	    	<div class="column">
+          <q-item-label 
+          	class="row justify-end"
+          	caption>
+          	{{ task.dueDate }}
+          </q-item-label>
+          <q-item-label
+          	class="row justify-end"
+          	caption>
+          	<small>{{ task.dueTime }}</small>
+          </q-item-label>
+	    	</div>
+    	</div>
     </q-item-section>
 
     <q-item-section side>
       <div class="row">
-        <div class="column justify-center">
-          <q-icon 
-            name="event"
-            size="18px"
-            class="q-mr-xs" />
-        </div>
-        <div class="column">
-          <q-item-label 
-            class="row justify-end"
-            caption>
-            {{ task.dueDate }}
-          </q-item-label>
-          <q-item-label
-            class="row justify-end"
-            caption>
-            <small>{{ task.dueTime }}</small>
-          </q-item-label>
-        </div>
+        <q-btn
+          @click.stop="showEditTask = true"
+          flat
+          round
+          dense
+          color="primary"
+          icon="edit" />
+        <q-btn
+          @click.stop="promptToDelete(id)"
+          flat
+          round
+          dense
+          color="red"
+          icon="delete" />
       </div>
     </q-item-section>
 
-    <q-item-section side>
-      <q-btn
-        @click.stop="promptToDelete(id)"
-        flat
-        round
-        dense
-        color="red"
-        icon="delete" />
-    </q-item-section>
+    <q-dialog v-model="showEditTask">
+      <edit-task 
+        @close="showEditTask = false" 
+        :task="task"
+        :id="id" />
+    </q-dialog>
     
   </q-item>
 </template>
@@ -56,8 +74,13 @@
 <script>
   import { mapActions } from 'vuex'
 
-  export default {
-    props: ['task', 'id'],
+	export default {
+		props: ['task', 'id'],
+    data() {
+      return {
+        showEditTask: false
+      }
+    },
     methods: {
       ...mapActions('tasks', ['updateTask', 'deleteTask']),
       promptToDelete(id) {
@@ -75,10 +98,13 @@
           this.deleteTask(id)
         })
       } 
+    },
+    components: {
+      'edit-task': require('components/Tasks/Modals/EditTask.vue').default
     }
-  }
+	}
 </script>
 
 <style>
-  
+	
 </style>
