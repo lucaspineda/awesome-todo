97dbffb54003428ebfe8f807c4e25658957c31a7
lucas7796 <lucas.pineda@hotmail.com>
2020-05-02 22:26:18 -0300

    implement new components in page todo component

--- src/pages/PageTodo.vue
+++ src/pages/PageTodo.vue
@@ -1,17 +1,16 @@
 <template>
   <q-page class="q-pa-md">
-		<q-list 
-			v-if="Object.keys(tasks).length"
-			separator
-			bordered>
+		
+		<no-tasks
+			v-if="!Object.keys(tasksTodo).length"></no-tasks>
 
-			<task
-				v-for="(task, key) in tasks"
-		  	:key="key"
-		  	:task="task"
-		  	:id="key"></task>
+		<tasks-todo
+			v-else
+			:tasksTodo="tasksTodo" />
 
-		</q-list>
+		<tasks-completed 
+			v-if="Object.keys(tasksCompleted).length"
+			:tasksCompleted="tasksCompleted" />
 
 		<div class="absolute-bottom text-center q-mb-lg">
 			<q-btn
@@ -39,11 +38,18 @@
 			}
 		},
 		computed: {
-			...mapGetters('tasks', ['tasks'])
+			...mapGetters('tasks', ['tasksTodo', 'tasksCompleted'])
+		},
+		mounted() {
+			this.$root.$on('showAddTask', () => {
+				this.showAddTask = true
+			})
 		},
 		components: {
-			'task' : require('components/Tasks/Task.vue').default,
 			'add-task' : require('components/Tasks/Modals/AddTask.vue').default,
+			'tasks-todo' : require('components/Tasks/TasksTodo.vue').default,
+			'tasks-completed' : require('components/Tasks/TasksCompleted.vue').default,
+			'no-tasks' : require('components/Tasks/NoTasks.vue').default
 		}
 	}
 </script>
