139e3964770be54926f830571067b7c4751b4cb8
lucas7796 <lucas.pineda@hotmail.com>
2020-05-04 23:52:04 -0300

    fix layout bug

--- src/pages/PageTodo.vue
+++ src/pages/PageTodo.vue
@@ -1,34 +1,40 @@
 <template>
-  <q-page class="q-pa-md">
+  <q-page>
+
+  	<div class="q-pa-md absolute full-width full-height column">
+  		<div class="row q-mb-lg">
+  			<search />
+  			<sort />
+  		</div>
+
+  		<p v-if="search && !Object.keys(tasksTodo).length && !Object.keys(tasksCompleted).length">No search results.</p>
+
+  		<q-scroll-area class="q-scroll-area-tasks">
+  			<no-tasks
+  				v-if="!Object.keys(tasksTodo).length && !search"></no-tasks>
+
+  			<tasks-todo
+  				v-if="Object.keys(tasksTodo).length"
+  				:tasksTodo="tasksTodo" />
+
+  			<tasks-completed 
+  				v-if="Object.keys(tasksCompleted).length"
+  				:tasksCompleted="tasksCompleted"
+  				class="q-mb-xl" />
+  		</q-scroll-area>
+
+  		<div class="absolute-bottom text-center q-mb-lg no-pointer-events">
+  			<q-btn
+  				@click="showAddTask = true"
+  			  round
+  			  class="all-pointer-events"
+  			  color="primary"
+  			  size="24px"
+  			  icon="add"
+  			/>
+  		</div>
+  	</div>
 		
-		<div class="row q-mb-lg">
-			<search />
-			<sort />
-		</div>
-
-		<p v-if="search && !Object.keys(tasksTodo).length && !Object.keys(tasksCompleted).length">No search results.</p>
-
-		<no-tasks
-			v-if="!Object.keys(tasksTodo).length && !search"></no-tasks>
-
-		<tasks-todo
-			v-if="Object.keys(tasksTodo).length"
-			:tasksTodo="tasksTodo" />
-
-		<tasks-completed 
-			v-if="Object.keys(tasksCompleted).length"
-			:tasksCompleted="tasksCompleted" />
-
-		<div class="absolute-bottom text-center q-mb-lg">
-			<q-btn
-				@click="showAddTask = true"
-			  round
-			  color="primary"
-			  size="24px"
-			  icon="add"
-			/>
-		</div>
-
 		<q-dialog v-model="showAddTask">
 		  <add-task @close="showAddTask = false" />
 		</q-dialog>
@@ -65,5 +71,8 @@
 </script>
 
 <style>
-	
+	.q-scroll-area-tasks {
+		display: flex;
+		flex-grow: 1;
+	}
 </style>
