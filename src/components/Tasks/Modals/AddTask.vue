db4d18bf270de6d3f2e6fb8c238b45708f79fdec
lucas7796 <lucas.pineda@hotmail.com>
2020-05-04 23:33:50 -0300

    implement the mixin in the components

--- src/components/Tasks/Modals/AddTask.vue
+++ src/components/Tasks/Modals/AddTask.vue
@@ -29,8 +29,10 @@
 
 <script>
 	import { mapActions } from 'vuex'
+	import mixinAddEditTask from 'src/mixins/mixin-add-edit-task'
 
 	export default {
+		mixins: [mixinAddEditTask],
 		data() {
 			return {
 				taskToSubmit: {
@@ -43,27 +45,10 @@
 		},
 		methods: {
 			...mapActions('tasks', ['addTask']),
-			submitForm() {
-				this.$refs.modalTaskName.$refs.name.validate()
-				if (!this.$refs.modalTaskName.$refs.name.hasError) {
-					this.submitTask()
-				}
-			},
 			submitTask() {
 				this.addTask(this.taskToSubmit)
 				this.$emit('close')
-			},
-			clearDueDate() {
-				this.taskToSubmit.dueDate = ''
-				this.taskToSubmit.dueTime = ''
 			}
-		},
-		components: {
-			'modal-header': require('components/Tasks/Modals/Shared/ModalHeader.vue').default,
-			'modal-task-name': require('components/Tasks/Modals/Shared/ModalTaskName.vue').default,
-			'modal-due-date': require('components/Tasks/Modals/Shared/ModalDueDate.vue').default,
-			'modal-due-time': require('components/Tasks/Modals/Shared/ModalDueTime.vue').default,
-			'modal-buttons': require('components/Tasks/Modals/Shared/ModalButtons.vue').default,
 		}
 	}
 </script>
--- src/components/Tasks/Modals/EditTask.vue
+++ src/components/Tasks/Modals/EditTask.vue
@@ -29,8 +29,10 @@
 
 <script>
 	import { mapActions } from 'vuex'
+	import mixinAddEditTask from 'src/mixins/mixin-add-edit-task'
 
 	export default {
+		mixins: [mixinAddEditTask],
 		props: ['task', 'id'],
 		data() {
 			return {
@@ -39,30 +41,13 @@
 		},
 		methods: {
 			...mapActions('tasks', ['updateTask']),
-			submitForm() {
-				this.$refs.modalTaskName.$refs.name.validate()
-				if (!this.$refs.modalTaskName.$refs.name.hasError) {
-					this.submitTask()
-				}
-			},
 			submitTask() {
 				this.updateTask({
 					id: this.id,
 					updates: this.taskToSubmit
 				})
 				this.$emit('close')
-			},
-			clearDueDate() {
-				this.taskToSubmit.dueDate = ''
-				this.taskToSubmit.dueTime = ''
 			}
-		},
-		components: {
-			'modal-header': require('components/Tasks/Modals/Shared/ModalHeader.vue').default,
-			'modal-task-name': require('components/Tasks/Modals/Shared/ModalTaskName.vue').default,
-			'modal-due-date': require('components/Tasks/Modals/Shared/ModalDueDate.vue').default,
-			'modal-due-time': require('components/Tasks/Modals/Shared/ModalDueTime.vue').default,
-			'modal-buttons': require('components/Tasks/Modals/Shared/ModalButtons.vue').default,
 		},
 		mounted() {
 			this.taskToSubmit = Object.assign({}, this.task)
