eb16ba4450c7385cb84390bbcdaa5a63b6991669
lucas7796 <lucas.pineda@hotmail.com>
2020-05-04 23:51:03 -0300

    add animation to both components

--- src/components/Tasks/TasksCompleted.vue
+++ src/components/Tasks/TasksCompleted.vue
@@ -1,19 +1,25 @@
 <template>
-	<div class="q-mt-lg">
-		<list-header
-			bgColor="bg-green-4">Completed</list-header>
-		<q-list 
-			separator
-			bordered>
+	<transition
+	  appear
+	  enter-active-class="animated zoomIn"
+	  leave-active-class="animated zoomOut"
+	>
+		<div class="q-mt-lg">
+			<list-header
+				bgColor="bg-green-4">Completed</list-header>
+			<q-list 
+				separator
+				bordered>
 
-			<task
-				v-for="(task, key) in tasksCompleted"
-		  	:key="key"
-		  	:task="task"
-		  	:id="key"></task>
+				<task
+					v-for="(task, key) in tasksCompleted"
+			  	:key="key"
+			  	:task="task"
+			  	:id="key"></task>
 
-		</q-list>
-	</div>
+			</q-list>
+		</div>
+	</transition>
 </template>
 
 <script>
--- src/components/Tasks/TasksTodo.vue
+++ src/components/Tasks/TasksTodo.vue
@@ -1,21 +1,27 @@
 <template>
-	<div>
-		<list-header
-			bgColor="bg-orange-4"
-			>Todo</list-header>
+	<transition
+	  appear
+	  enter-active-class="animated zoomIn"
+	  leave-active-class="animated zoomOut absolute-top"
+	>
+		<div>
+			<list-header
+				bgColor="bg-orange-4"
+				>Todo</list-header>
 
-		<q-list 
-			separator
-			bordered>
+			<q-list 
+				separator
+				bordered>
 
-			<task
-				v-for="(task, key) in tasksTodo"
-		  	:key="key"
-		  	:task="task"
-		  	:id="key"></task>
+				<task
+					v-for="(task, key) in tasksTodo"
+			  	:key="key"
+			  	:task="task"
+			  	:id="key"></task>
 
-		</q-list>
-	</div>
+			</q-list>
+		</div>
+	</transition>
 </template>
 
 <script>
