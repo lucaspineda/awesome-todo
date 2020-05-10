112efbb94213ca9f0afbfe908f7327a304fa8b14
lucas7796 <lucas.pineda@hotmail.com>
2020-05-10 00:00:41 -0300

    show chosen option on tasks page

--- src/components/Tasks/Task.vue
+++ src/components/Tasks/Task.vue
@@ -37,7 +37,7 @@
           <q-item-label
           	class="row justify-end"
           	caption>
-          	<small>{{ task.dueTime }}</small>
+          	<small>{{ taskDueTime }}</small>
           </q-item-label>
 	    	</div>
     	</div>
@@ -73,7 +73,7 @@
 </template>
 
 <script>
-  import { mapState, mapActions } from 'vuex'
+  import { mapState, mapActions, mapGetters } from 'vuex'
   import { date } from 'quasar'
 
 	export default {
@@ -84,7 +84,14 @@
       }
     },
     computed: {
-      ...mapState('tasks', ['search'])
+      ...mapState('tasks', ['search']),
+      ...mapGetters('settings', ['settings']),
+      taskDueTime() {
+        if (this.settings.show12HourTimeFormat) {
+          return date.formatDate(this.task.dueDate + ' ' + this.task.dueTime, 'h:mmA')
+        }
+        return this.task.dueTime
+      }
     },
     methods: {
       ...mapActions('tasks', ['updateTask', 'deleteTask']),
