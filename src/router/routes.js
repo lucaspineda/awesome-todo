d7e3331575ff00f923779092ee71b379dfd0c559
Lucas Pineda <lucas.pineda@hotmail.com>
2020-06-18 21:56:39 -0300

    create a help page

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,13 +5,17 @@ const routes = [
     component: () => import('layouts/Layout.vue'),
     children: [
       { 
-        path: '',
+        path: '', 
         component: () => import('pages/PageTodo.vue') 
       },
       { 
-        path: '/settings',
+        path: '/settings', 
         component: () => import('pages/PageSettings.vue') 
       },
+      { 
+        path: '/settings/help', 
+        component: () => import('pages/PageHelp.vue') 
+      } 
     ]
   }
 ]
